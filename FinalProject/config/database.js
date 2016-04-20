24dfac3eb4f760ff2db4a026d57c4e50d39831e1
Grant Baugh <grantb4102@gmail.com>
2016-04-20 15:21:38 -0600

    Made images actually work

--- FinalProject/config/database.js
+++ FinalProject/config/database.js
@@ -1,3 +1,9 @@
 /**
  * Created by Grant on 4/9/2016.
  */
+module.exports = {
+
+    //'url' : 'C:/Program Files/MongoDB/Server/3.2/bin/mongo'
+    //'url' : "mongodb://C:/Program Files/MongoDB/Server/3.2/bin/mongo"
+
+};
--- UserManagementWebSystem/config/database.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Grant on 4/9/2016.
- */
